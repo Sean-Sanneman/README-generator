e017ec514a23a0e80838578a1392adcbad289de5
Sean_Dafydd <seandafydd@gmail.com>
2020-07-17 23:15:09 -0700

    completed questions for input array

--- Develop/index.js
+++ Develop/index.js
@@ -10,6 +10,48 @@ const questions = [
     name: "github",
     message: "Hello, what is your github username?",
   },
+  {
+    type: "input",
+    name: "title",
+    default: "My Project"
+    message: "Nice to meet you! What is the Title of this project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    default: "none",
+    message: "Please enter a detailed description of your project. You may want to include information about what languages/tech were used in the creation of your project, or what inspired you to build it, etc.",
+  },
+  {
+    type: "input",
+    name: "table",
+    default: "none",
+    message: "If you would like to enter a table of contents, please do so here.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    default: "none",
+    message: "How can people install your project? Please provide a step-by-step description of how to get the development environment running.", 
+  },
+  {
+    type: "input",
+    name: "usage",
+    default: "none",
+    message: "Please provide instructions and examples for use. This would be a good place to add screenshots if you like.",
+  },
+  {
+    type: "input",
+    name: "credits",
+    default: "none",
+    message: "Please provide a list of who contributed to this project, and include links to their GitHub profiles. If you used any third party assets that require attribution, make sure you list them along with a link to their web page.",
+  },
+  {
+    type: "checkbox",
+    name: "rights",
+    default: "MIT License",
+    message: "What kind of license is this project released under?",
+  },
 ];
 
 // function to write README file
@@ -25,7 +67,7 @@ function init() {
     // getting response
 
     .then((response) => {
-      console.log("Sit tight, crunch, crunch.");
+      console.log("Sit tight, crunch, crunch, crunch...");
       writeToFile("README.md", generateMarkdown({ ...response }));
     });
 }
